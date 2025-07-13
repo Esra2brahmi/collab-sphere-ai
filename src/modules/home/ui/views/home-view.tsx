bf7b5d1f14a8dbd63bcfb11cc93033b09e50cbd1
Esra2brahmi <esrabrahmii@gmail.com>
2025-07-13 14:12:06 +0100

    09-agents-setup

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,19 +1,13 @@
 "use client";
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-
-
 
 
 
 export const HomeView= () =>{
-  const trpc=useTRPC();
-  const {data}=useQuery(trpc.hello.queryOptions({text:"issra"}));
  
     return (
-    <div className="flax fax-col p-4 gap-y-4">
-       {data?.greeting}
+    <div>
+       Home view
     </div>
     );
 }
